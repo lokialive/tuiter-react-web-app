648477c57bce0d0a59435281a0b21b8a361c434e
yfqiao <qiao.yif@northeastern.edu>
2022-11-21 02:30:15 -0500

    update api link

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,28 +1,25 @@
-import axios from 'axios';
-const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
-
+import axios from 'axios'
+// const API_BASE = process.env.REACT_APP_API_BASE;
+// const TUITS_API = `${API_BASE}/tuits`;
+const TUITS_API = 'https://tuiter-node-server-app-yifan.herokuapp.com/api/tuits'
 export const createTuit = async (tuit) => {
-    const response = await axios.post(TUITS_API, tuit)
-    return response.data;
+  const response = await axios.post(TUITS_API, tuit)
+  return response.data
 }
 
 export const deleteTuit = async (tid) => {
-    const response = await axios
-        .delete(`${TUITS_API}/${tid}`)
-    return response.data
+  const response = await axios.delete(`${TUITS_API}/${tid}`)
+  return response.data
 }
 
 export const updateTuit = async (tuit) => {
-    const response = await axios
-        .put(`${TUITS_API}/${tuit._id}`, tuit);
-    console.log(response)
-    return tuit;
+  const response = await axios.put(`${TUITS_API}/${tuit._id}`, tuit)
+  console.log(response)
+  return tuit
 }
 
-
 export const findTuits = async () => {
-    const response = await axios.get(TUITS_API);
-    const tuits = response.data;
-    return tuits;
+  const response = await axios.get(TUITS_API)
+  const tuits = response.data
+  return tuits
 }
