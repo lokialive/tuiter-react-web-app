499065c4d8b2169ab7220350c9b16890d956af3b
yfqiao <qiao.yif@northeastern.edu>
2022-12-09 19:40:12 -0800

    update a9

--- src/tuiter/reducers/who-reducer.js
+++ src/tuiter/reducers/who-reducer.js
@@ -1,9 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
-import whoArray from "../data/Who.json";
+import { createSlice } from '@reduxjs/toolkit'
 
 const whoSlice = createSlice({
-  name: "who",
-  initialState: whoArray,
-});
+  name: 'who',
+  initialState: [
+    {
+      avatarIcon: '/tuiter/images/java.jpeg',
+      userName: 'Java',
+      handle: 'Java',
+    },
+    {
+      avatarIcon: '/tuiter/images/relativity_space.jpeg',
+      userName: 'Relativity Space',
+      handle: 'relativityspace',
+    },
+    {
+      avatarIcon: '/tuiter/images/virgin.png',
+      userName: 'Virgin Galactic',
+      handle: 'virgingalactic',
+    },
+    {
+      avatarIcon: '/tuiter/images/nasa.png',
+      userName: 'NASA',
+      handle: 'nasa',
+    },
+    {
+      avatarIcon: '/tuiter/images/tesla.jpeg',
+      userName: 'Tesla',
+      handle: 'tesla',
+    },
+  ],
+})
 
-export default whoSlice.reducer;
+export default whoSlice.reducer
