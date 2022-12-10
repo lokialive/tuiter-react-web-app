f393d7d562607ea5d81f11f30c906001bb26fdd7
yfqiao <qiao.yif@northeastern.edu>
2022-12-09 19:45:11 -0800

    update a9

--- src/tuiter/post-list/index.js
+++ src/tuiter/post-list/index.js
@@ -1,15 +1,47 @@
-import PostItem from "./PostItem.js";
-import Posts from "./Posts.json";
-import React from "react";
+import PostItem from './PostItem.js'
+import React from 'react'
 
 const PostList = () => {
+  let Posts = [
+    {
+      author: 'Elon Musk',
+      userName: 'elonmusk',
+      time: '23h',
+      title: "Amazing show about <a href='#'>@inspiration4x</a> mission!",
+      avatarImage: '/tuiter/images/elon_musk.jpeg',
+      postImageUrl: '/tuiter/images/astronaut.jpeg',
+      postContent: {
+        postHeading:
+          'Countdown: Inspirational Mission to Space | Netflix Official site',
+        postContent:
+          'From training to launch to landing, this all-access docuseries rides along with the Inpiration4 crew on the first all-civilian orbital space ...',
+        postSiteLink: 'netflix.com',
+      },
+      numberOfComments: '4.2k',
+      numberOfRetweets: '3.5k',
+      numberOfLikes: '37.5k',
+    },
+    {
+      author: 'New York Post',
+      userName: 'nypost',
+      time: '23h',
+      title:
+        "Grimes reading Karl Marx following split with world's reichest man Elon musk <a href='#'>trib.al/nx2Gfaq</a>",
+      avatarImage: '/tuiter/images/new_york_post.jpeg',
+      postImageUrl: '/tuiter/images/book_read.jpeg',
+      numberOfComments: '965',
+      numberOfRetweets: '2.4k',
+      numberOfLikes: '4k',
+    },
+  ]
+
   return (
     <ul class="list-group">
       {Posts.map((post) => {
-        console.log(post);
-        return <PostItem post={post} />;
+        console.log(post)
+        return <PostItem post={post} />
       })}
     </ul>
-  );
-};
-export default PostList;
+  )
+}
+export default PostList
--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -1,14 +1,50 @@
-import PostSummaryItem from "./PostSummaryItem";
-import Posts from "./Posts.json";
-import React from "react";
+import PostSummaryItem from './PostSummaryItem'
+import Posts from './Posts.json'
+import React from 'react'
 
 const PostSummaryList = () => {
+  let Posts = [
+    {
+      topic: 'Web Development',
+      userName: 'ReactJS',
+      time: '2h',
+      title:
+        'React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs',
+      image: '/tuiter/images/react.png',
+    },
+    {
+      topic: '',
+      userName: 'JavaScript',
+      time: '1 day',
+      title:
+        'JavaScript is programming language that can run on browsers as well as desktops',
+      image: '/tuiter/images/js.png',
+      tweets: '123K',
+    },
+    {
+      topic: 'Web Development',
+      userName: '',
+      title: 'jQuery',
+      time: 'last week',
+      image: '/tuiter/images/jquery.png',
+      tweets: '122K',
+    },
+    {
+      topic: 'Web Development',
+      userName: 'NodeJS',
+      title: '',
+      time: 'last month',
+      image: '/tuiter/images/nodejs.png',
+      tweets: '120K',
+    },
+  ]
+
   return (
     <ul className="list-group">
       {Posts.map((post) => (
         <PostSummaryItem post={post} />
       ))}
     </ul>
-  );
-};
-export default PostSummaryList;
+  )
+}
+export default PostSummaryList
